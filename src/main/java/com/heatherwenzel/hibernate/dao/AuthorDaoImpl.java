e5510d9a7a5a23abb519126bc9d5082046b2ff1e
Heather Wenzel <heather.wenzel@ge.com>
2022-02-02 09:28:34 -0500

    Implement findAuthorByName method

--- src/main/java/com/heatherwenzel/hibernate/dao/AuthorDaoImpl.java
+++ src/main/java/com/heatherwenzel/hibernate/dao/AuthorDaoImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
@@ -22,7 +23,13 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        return null;
+        TypedQuery<Author> query = getEntityManager().createQuery("select a from author a " +
+                "where a.firstName = :first_name and a.lastName = :last_name", Author.class);
+
+        query.setParameter("first_name", firstName);
+        query.setParameter("last_name", lastName);
+
+        return query.getSingleResult();
     }
 
     @Override
